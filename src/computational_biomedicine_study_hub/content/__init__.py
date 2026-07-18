5128f3a1b274b0a4517fc8a95f9a32e0e655be9e
JennerFeijoo <65651751+jennerfeijoo@users.noreply.github.com>
2026-07-18 17:00:12 +0200

    export localized academic content contracts

--- src/computational_biomedicine_study_hub/content/__init__.py
+++ src/computational_biomedicine_study_hub/content/__init__.py
@@ -2,6 +2,17 @@
 
 from __future__ import annotations
 
+from .localized_models import (
+    LocalizedAssessmentItem,
+    LocalizedAssessmentOption,
+    LocalizedConceptBlock,
+    LocalizedLearningModule,
+    LocalizedLearningObjective,
+    LocalizedPracticeExercise,
+    LocalizedText,
+    LocalizedTutorSupportPacket,
+    LocalizedWorkedExample,
+)
 from .models import (
     AssessmentItem,
     ConceptBlock,
@@ -18,6 +29,15 @@ __all__ = [
     "ConceptBlock",
     "LearningModule",
     "LearningObjective",
+    "LocalizedAssessmentItem",
+    "LocalizedAssessmentOption",
+    "LocalizedConceptBlock",
+    "LocalizedLearningModule",
+    "LocalizedLearningObjective",
+    "LocalizedPracticeExercise",
+    "LocalizedText",
+    "LocalizedTutorSupportPacket",
+    "LocalizedWorkedExample",
     "PracticeExercise",
     "TutorKnowledgeDocument",
     "TutorSupportPacket",
