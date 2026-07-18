f169c8416bdc166ed78e5588cd87bcf310d6f405
JennerFeijoo <65651751+jennerfeijoo@users.noreply.github.com>
2026-07-18 14:35:29 +0200

    export the DM857 module 1 objective bank

--- src/computational_biomedicine_study_hub/content/dm857/__init__.py
+++ src/computational_biomedicine_study_hub/content/dm857/__init__.py
@@ -3,7 +3,8 @@
 from __future__ import annotations
 
 from .module_01_foundations import MODULE as MODULE_01_FOUNDATIONS
+from .module_01_objective_bank import OBJECTIVE_QUESTION_BANK
 
 MODULES = (MODULE_01_FOUNDATIONS,)
 
-__all__ = ["MODULES", "MODULE_01_FOUNDATIONS"]
+__all__ = ["MODULES", "MODULE_01_FOUNDATIONS", "OBJECTIVE_QUESTION_BANK"]
