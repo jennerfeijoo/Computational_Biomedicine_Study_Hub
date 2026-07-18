7ecdcd6af28407564aeeb87b377be3b9cfaa2120
JennerFeijoo <65651751+jennerfeijoo@users.noreply.github.com>
2026-07-18 14:39:22 +0200

    export DM857 module 1 assessment bank

--- src/computational_biomedicine_study_hub/content/dm857/__init__.py
+++ src/computational_biomedicine_study_hub/content/dm857/__init__.py
@@ -2,8 +2,13 @@
 
 from __future__ import annotations
 
+from .module_01_assessment_bank import EXTRA_CLOSED_ASSESSMENT_ITEMS
 from .module_01_foundations import MODULE as MODULE_01_FOUNDATIONS
 
 MODULES = (MODULE_01_FOUNDATIONS,)
 
-__all__ = ["MODULES", "MODULE_01_FOUNDATIONS"]
+__all__ = [
+    "EXTRA_CLOSED_ASSESSMENT_ITEMS",
+    "MODULES",
+    "MODULE_01_FOUNDATIONS",
+]
