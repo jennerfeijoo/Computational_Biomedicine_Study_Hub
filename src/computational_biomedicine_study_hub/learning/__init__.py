9965e58f72460de391f6c629c4f345e70e365bd9
JennerFeijoo <65651751+jennerfeijoo@users.noreply.github.com>
2026-07-18 14:37:29 +0200

    export objective assessment domain primitives

--- src/computational_biomedicine_study_hub/learning/__init__.py
+++ src/computational_biomedicine_study_hub/learning/__init__.py
@@ -1,5 +1,20 @@
 """Learning-domain primitives shared by courses and assessment features."""
 
 from .activity_types import ActivityType, StudyCycleStage
+from .objective_assessment import (
+    ObjectiveAnswerFeedback,
+    ObjectiveAssessmentSession,
+    ObjectiveSessionGenerator,
+    ObjectiveSessionQuestion,
+    grade_objective_answer,
+)
 
-__all__ = ["ActivityType", "StudyCycleStage"]
+__all__ = [
+    "ActivityType",
+    "ObjectiveAnswerFeedback",
+    "ObjectiveAssessmentSession",
+    "ObjectiveSessionGenerator",
+    "ObjectiveSessionQuestion",
+    "StudyCycleStage",
+    "grade_objective_answer",
+]
