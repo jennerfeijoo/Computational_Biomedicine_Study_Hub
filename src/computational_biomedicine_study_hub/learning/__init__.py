e364eeed41a7b645e75f9cb5d96eb7990b838d4a
JennerFeijoo <65651751+jennerfeijoo@users.noreply.github.com>
2026-07-30 21:42:04 +0200

    Export learning progress primitives

--- src/computational_biomedicine_study_hub/learning/__init__.py
+++ src/computational_biomedicine_study_hub/learning/__init__.py
@@ -1,5 +1,12 @@
 """Learning-domain primitives shared by courses and assessment features."""
 
 from .activity_types import ActivityType, StudyCycleStage
+from .progress import AttemptRecord, ConfidenceLevel, MasteryState
 
-__all__ = ["ActivityType", "StudyCycleStage"]
+__all__ = [
+    "ActivityType",
+    "AttemptRecord",
+    "ConfidenceLevel",
+    "MasteryState",
+    "StudyCycleStage",
+]
