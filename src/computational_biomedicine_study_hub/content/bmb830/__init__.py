fd53580ea3d876a033676a8dc859e7f749c88a78
JennerFeijoo <65651751+jennerfeijoo@users.noreply.github.com>
2026-07-31 19:44:02 +0200

    Register BMB830 module bundles

---
+++ src/computational_biomedicine_study_hub/content/bmb830/__init__.py
@@ -0,0 +1,78 @@
+"""Trilingual authored content and runtime bundles for BMB830."""
+
+from __future__ import annotations
+
+from ...i18n import AppLocale
+from ..bundles import LocalizedModuleBundle, validate_bundle_catalog
+from .module_01_r_foundations import (
+    LOCALIZED_MODULE_01_R_FOUNDATIONS,
+    LOCALIZED_OBJECTIVE_QUESTION_BANK_01,
+    materialize_module_01_question_bank,
+)
+from .module_02_data_summary import (
+    LOCALIZED_MODULE_02_DATA_SUMMARY,
+    LOCALIZED_OBJECTIVE_QUESTION_BANK_02,
+    materialize_module_02_question_bank,
+)
+from .module_03_probability import (
+    LOCALIZED_MODULE_03_PROBABILITY,
+    LOCALIZED_OBJECTIVE_QUESTION_BANK_03,
+    materialize_module_03_question_bank,
+)
+
+LOCALIZED_BUNDLES = (
+    LocalizedModuleBundle(
+        LOCALIZED_MODULE_01_R_FOUNDATIONS,
+        LOCALIZED_OBJECTIVE_QUESTION_BANK_01,
+        "1.0.0",
+    ),
+    LocalizedModuleBundle(
+        LOCALIZED_MODULE_02_DATA_SUMMARY,
+        LOCALIZED_OBJECTIVE_QUESTION_BANK_02,
+        "1.0.0",
+    ),
+    LocalizedModuleBundle(
+        LOCALIZED_MODULE_03_PROBABILITY,
+        LOCALIZED_OBJECTIVE_QUESTION_BANK_03,
+        "1.0.0",
+    ),
+)
+validate_bundle_catalog(LOCALIZED_BUNDLES)
+
+BUNDLES = tuple(bundle.materialize(AppLocale.SPANISH_SPAIN) for bundle in LOCALIZED_BUNDLES)
+LOCALIZED_MODULES = tuple(bundle.localized_module for bundle in LOCALIZED_BUNDLES)
+MODULES = tuple(bundle.module for bundle in BUNDLES)
+OBJECTIVE_QUESTION_BANKS = {
+    bundle.module.module_id: bundle.objective_question_bank for bundle in BUNDLES
+}
+
+MODULE_01_R_FOUNDATIONS = BUNDLES[0].module
+MODULE_02_DATA_SUMMARY = BUNDLES[1].module
+MODULE_03_PROBABILITY = BUNDLES[2].module
+
+OBJECTIVE_QUESTION_BANK_01 = BUNDLES[0].objective_question_bank
+OBJECTIVE_QUESTION_BANK_02 = BUNDLES[1].objective_question_bank
+OBJECTIVE_QUESTION_BANK_03 = BUNDLES[2].objective_question_bank
+
+__all__ = [
+    "BUNDLES",
+    "LOCALIZED_BUNDLES",
+    "LOCALIZED_MODULES",
+    "LOCALIZED_MODULE_01_R_FOUNDATIONS",
+    "LOCALIZED_MODULE_02_DATA_SUMMARY",
+    "LOCALIZED_MODULE_03_PROBABILITY",
+    "LOCALIZED_OBJECTIVE_QUESTION_BANK_01",
+    "LOCALIZED_OBJECTIVE_QUESTION_BANK_02",
+    "LOCALIZED_OBJECTIVE_QUESTION_BANK_03",
+    "MODULES",
+    "MODULE_01_R_FOUNDATIONS",
+    "MODULE_02_DATA_SUMMARY",
+    "MODULE_03_PROBABILITY",
+    "OBJECTIVE_QUESTION_BANKS",
+    "OBJECTIVE_QUESTION_BANK_01",
+    "OBJECTIVE_QUESTION_BANK_02",
+    "OBJECTIVE_QUESTION_BANK_03",
+    "materialize_module_01_question_bank",
+    "materialize_module_02_question_bank",
+    "materialize_module_03_question_bank",
+]
