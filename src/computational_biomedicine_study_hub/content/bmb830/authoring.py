fd8de9f1e583bbc2350ae13d41cbd705d6a2b227
JennerFeijoo <65651751+jennerfeijoo@users.noreply.github.com>
2026-07-31 19:32:15 +0200

    Add BMB830 authoring helpers

---
+++ src/computational_biomedicine_study_hub/content/bmb830/authoring.py
@@ -0,0 +1,33 @@
+"""BMB830-local access to shared trilingual authoring helpers."""
+
+from __future__ import annotations
+
+from ..dm857.authoring import (
+    Triple,
+    authored_item,
+    concept,
+    example,
+    objective,
+    objective_mcq,
+    objective_tf,
+    option,
+    practice,
+    same,
+    t,
+    tutor_support,
+)
+
+__all__ = [
+    "Triple",
+    "authored_item",
+    "concept",
+    "example",
+    "objective",
+    "objective_mcq",
+    "objective_tf",
+    "option",
+    "practice",
+    "same",
+    "t",
+    "tutor_support",
+]
