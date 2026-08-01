20a4ceacd1b9a0cecb74e3a9e02ddd73abd61613
JennerFeijoo <65651751+jennerfeijoo@users.noreply.github.com>
2026-08-01 10:16:50 +0200

    Register BMB831 omics modules

--- src/computational_biomedicine_study_hub/content/bmb831/__init__.py
+++ src/computational_biomedicine_study_hub/content/bmb831/__init__.py
@@ -9,11 +9,31 @@ from .module_01_synthea_workflows import (
     LOCALIZED_OBJECTIVE_QUESTION_BANK_01,
     materialize_module_01_question_bank,
 )
+from .module_02_omics_matrices_qc import (
+    LOCALIZED_MODULE_02_OMICS_MATRICES_QC,
+    LOCALIZED_OBJECTIVE_QUESTION_BANK_02,
+    materialize_module_02_question_bank,
+)
+from .module_03_differential_modeling import (
+    LOCALIZED_MODULE_03_DIFFERENTIAL_MODELING,
+    LOCALIZED_OBJECTIVE_QUESTION_BANK_03,
+    materialize_module_03_question_bank,
+)
 
 LOCALIZED_BUNDLES = (
     LocalizedModuleBundle(
         LOCALIZED_MODULE_01_SYNTHEA_WORKFLOWS,
         LOCALIZED_OBJECTIVE_QUESTION_BANK_01,
+        "1.0.0",
+    ),
+    LocalizedModuleBundle(
+        LOCALIZED_MODULE_02_OMICS_MATRICES_QC,
+        LOCALIZED_OBJECTIVE_QUESTION_BANK_02,
+        "1.0.0",
+    ),
+    LocalizedModuleBundle(
+        LOCALIZED_MODULE_03_DIFFERENTIAL_MODELING,
+        LOCALIZED_OBJECTIVE_QUESTION_BANK_03,
         "1.0.0",
     ),
 )
@@ -27,17 +47,31 @@ OBJECTIVE_QUESTION_BANKS = {
 }
 
 MODULE_01_SYNTHEA_WORKFLOWS = BUNDLES[0].module
+MODULE_02_OMICS_MATRICES_QC = BUNDLES[1].module
+MODULE_03_DIFFERENTIAL_MODELING = BUNDLES[2].module
 OBJECTIVE_QUESTION_BANK_01 = BUNDLES[0].objective_question_bank
+OBJECTIVE_QUESTION_BANK_02 = BUNDLES[1].objective_question_bank
+OBJECTIVE_QUESTION_BANK_03 = BUNDLES[2].objective_question_bank
 
 __all__ = [
     "BUNDLES",
     "LOCALIZED_BUNDLES",
     "LOCALIZED_MODULES",
     "LOCALIZED_MODULE_01_SYNTHEA_WORKFLOWS",
+    "LOCALIZED_MODULE_02_OMICS_MATRICES_QC",
+    "LOCALIZED_MODULE_03_DIFFERENTIAL_MODELING",
     "LOCALIZED_OBJECTIVE_QUESTION_BANK_01",
+    "LOCALIZED_OBJECTIVE_QUESTION_BANK_02",
+    "LOCALIZED_OBJECTIVE_QUESTION_BANK_03",
     "MODULES",
     "MODULE_01_SYNTHEA_WORKFLOWS",
+    "MODULE_02_OMICS_MATRICES_QC",
+    "MODULE_03_DIFFERENTIAL_MODELING",
     "OBJECTIVE_QUESTION_BANKS",
     "OBJECTIVE_QUESTION_BANK_01",
+    "OBJECTIVE_QUESTION_BANK_02",
+    "OBJECTIVE_QUESTION_BANK_03",
     "materialize_module_01_question_bank",
+    "materialize_module_02_question_bank",
+    "materialize_module_03_question_bank",
 ]
