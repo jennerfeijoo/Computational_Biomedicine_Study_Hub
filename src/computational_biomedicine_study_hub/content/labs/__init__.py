782f24b839a39fac06d2f50ea38c30d916d2c7b3
JennerFeijoo <65651751+jennerfeijoo@users.noreply.github.com>
2026-08-04 15:09:03 +0200

    Register the DM847 short-read mapping laboratory

--- src/computational_biomedicine_study_hub/content/labs/__init__.py
+++ src/computational_biomedicine_study_hub/content/labs/__init__.py
@@ -1,7 +1,8 @@
 """Authored internal preparation laboratories for computational biomedicine."""
 
+from .dm847_lab_01 import DM847_LAB_01
 from .dm857_lab_01 import DM857_LAB_01
 
-LABS = (DM857_LAB_01,)
+LABS = (DM857_LAB_01, DM847_LAB_01)
 
-__all__ = ["DM857_LAB_01", "LABS"]
+__all__ = ["DM847_LAB_01", "DM857_LAB_01", "LABS"]
