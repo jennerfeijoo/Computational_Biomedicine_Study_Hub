5c6a1e9a44a79a966dbef162f658dfba8ae0c120
JennerFeijoo <65651751+jennerfeijoo@users.noreply.github.com>
2026-08-04 21:15:19 +0200

    Assemble split DM847 technical station modules

--- src/computational_biomedicine_study_hub/content/technical_stations/__init__.py
+++ src/computational_biomedicine_study_hub/content/technical_stations/__init__.py
@@ -1,32 +1,22 @@
 """Authored artifact-based technical reasoning stations."""
 
-from dataclasses import replace
+from .dm847_alignment import DM847_ALIGNMENT_STATIONS
+from .dm847_hmm import DM847_HMM_STATIONS
+from .dm847_index import DM847_INDEX_STATIONS
+from .dm847_mapping import DM847_MAPPING_STATIONS
 
-from .dm847 import DM847_TECHNICAL_STATIONS as _DM847_RAW_STATIONS
-
-_LAB_ID_ALIASES = {
-    "dm847.lab01.sequence-mapping": "dm847.lab01.short-read-mapping",
-    "dm847.lab03.sequence-index": "dm847.lab03.sequence-indexes",
-    "dm847.lab04.hidden-markov-model": "dm847.lab04.hidden-markov-models",
-}
-
-DM847_TECHNICAL_STATIONS = tuple(
-    replace(
-        station,
-        lab_id=_LAB_ID_ALIASES.get(station.lab_id, station.lab_id),
-        source_basis=tuple(_LAB_ID_ALIASES.get(item, item) for item in station.source_basis),
-    )
-    for station in _DM847_RAW_STATIONS
+DM847_TECHNICAL_STATIONS = (
+    *DM847_MAPPING_STATIONS,
+    *DM847_ALIGNMENT_STATIONS,
+    *DM847_INDEX_STATIONS,
+    *DM847_HMM_STATIONS,
 )
 
 STATIONS_BY_LAB = {
-    lab_id: tuple(station for station in DM847_TECHNICAL_STATIONS if station.lab_id == lab_id)
-    for lab_id in {
-        "dm847.lab01.short-read-mapping",
-        "dm847.lab02.pairwise-alignment",
-        "dm847.lab03.sequence-indexes",
-        "dm847.lab04.hidden-markov-models",
-    }
+    "dm847.lab01.short-read-mapping": DM847_MAPPING_STATIONS,
+    "dm847.lab02.pairwise-alignment": DM847_ALIGNMENT_STATIONS,
+    "dm847.lab03.sequence-indexes": DM847_INDEX_STATIONS,
+    "dm847.lab04.hidden-markov-models": DM847_HMM_STATIONS,
 }
 
 __all__ = ["DM847_TECHNICAL_STATIONS", "STATIONS_BY_LAB"]
