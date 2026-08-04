a05192676bae2e997771612f26e383eb723f7856
JennerFeijoo <65651751+jennerfeijoo@users.noreply.github.com>
2026-08-04 15:10:24 +0200

    Expose the combined laboratory and workspace catalogs

--- src/computational_biomedicine_study_hub/content/labs/__init__.py
+++ src/computational_biomedicine_study_hub/content/labs/__init__.py
@@ -2,7 +2,16 @@
 
 from .dm847_lab_01 import DM847_LAB_01
 from .dm857_lab_01 import DM857_LAB_01
+from .workspace_catalog import WORKSPACE_TEMPLATES
+from .workspaces import WORKSPACE_TEMPLATES as _LEGACY_WORKSPACE_TEMPLATES
+
+_LEGACY_WORKSPACE_TEMPLATES.update(WORKSPACE_TEMPLATES)
 
 LABS = (DM857_LAB_01, DM847_LAB_01)
 
-__all__ = ["DM847_LAB_01", "DM857_LAB_01", "LABS"]
+__all__ = [
+    "DM847_LAB_01",
+    "DM857_LAB_01",
+    "LABS",
+    "WORKSPACE_TEMPLATES",
+]
