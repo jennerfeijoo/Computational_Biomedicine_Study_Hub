ab0bc5f8cf206d699fecdb6459656360d814aae8
JennerFeijoo <65651751+jennerfeijoo@users.noreply.github.com>
2026-08-04 15:31:33 +0200

    Register the DM847 alignment workspace

--- src/computational_biomedicine_study_hub/content/labs/workspace_catalog.py
+++ src/computational_biomedicine_study_hub/content/labs/workspace_catalog.py
@@ -3,15 +3,18 @@
 from __future__ import annotations
 
 from .dm847_workspace_01 import DM847_LAB_01_WORKSPACE
+from .dm847_workspace_02 import DM847_LAB_02_WORKSPACE
 from .workspaces import DM857_LAB_01_WORKSPACE
 
 WORKSPACE_TEMPLATES = {
     DM857_LAB_01_WORKSPACE.lab_id: DM857_LAB_01_WORKSPACE,
     DM847_LAB_01_WORKSPACE.lab_id: DM847_LAB_01_WORKSPACE,
+    DM847_LAB_02_WORKSPACE.lab_id: DM847_LAB_02_WORKSPACE,
 }
 
 __all__ = [
     "DM847_LAB_01_WORKSPACE",
+    "DM847_LAB_02_WORKSPACE",
     "DM857_LAB_01_WORKSPACE",
     "WORKSPACE_TEMPLATES",
 ]
