cd3f7a67ff125cf8e5352d6117e4fdb33e06e6ba
JennerFeijoo <65651751+jennerfeijoo@users.noreply.github.com>
2026-08-04 19:38:45 +0200

    Register DM847 hidden Markov model laboratory

--- src/computational_biomedicine_study_hub/content/labs/__init__.py
+++ src/computational_biomedicine_study_hub/content/labs/__init__.py
@@ -3,18 +3,26 @@
 from .dm847_lab_01 import DM847_LAB_01
 from .dm847_lab_02 import DM847_LAB_02
 from .dm847_lab_03 import DM847_LAB_03
+from .dm847_lab_04 import DM847_LAB_04
 from .dm857_lab_01 import DM857_LAB_01
 from .workspace_catalog import WORKSPACE_TEMPLATES
 from .workspaces import WORKSPACE_TEMPLATES as _LEGACY_WORKSPACE_TEMPLATES
 
 _LEGACY_WORKSPACE_TEMPLATES.update(WORKSPACE_TEMPLATES)
 
-LABS = (DM857_LAB_01, DM847_LAB_01, DM847_LAB_02, DM847_LAB_03)
+LABS = (
+    DM857_LAB_01,
+    DM847_LAB_01,
+    DM847_LAB_02,
+    DM847_LAB_03,
+    DM847_LAB_04,
+)
 
 __all__ = [
     "DM847_LAB_01",
     "DM847_LAB_02",
     "DM847_LAB_03",
+    "DM847_LAB_04",
     "DM857_LAB_01",
     "LABS",
     "WORKSPACE_TEMPLATES",
