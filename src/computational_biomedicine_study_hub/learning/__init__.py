c37a33ecba293cee70681ed545b970959e5671b9
JennerFeijoo <65651751+jennerfeijoo@users.noreply.github.com>
2026-08-04 05:55:57 +0200

    Export learning-path domain primitives

--- src/computational_biomedicine_study_hub/learning/__init__.py
+++ src/computational_biomedicine_study_hub/learning/__init__.py
@@ -1,6 +1,16 @@
 """Learning-domain primitives shared by courses and assessment features."""
 
 from .activity_types import ActivityType, StudyCycleStage
+from .pathway import (
+    DEFAULT_COURSE_PLANS,
+    CourseModulePlan,
+    LearningDestination,
+    LearningDestinationKind,
+    LearningPathEngine,
+    LearningPathRecommendation,
+    LearningPathSnapshot,
+    RecommendationReason,
+)
 from .progress import AttemptRecord, ConfidenceLevel, MasteryState
 from .python_execution import (
     ExecutionStatus,
@@ -11,14 +21,22 @@ from .python_execution import (
 )
 
 __all__ = [
+    "DEFAULT_COURSE_PLANS",
     "ActivityType",
     "AttemptRecord",
     "ConfidenceLevel",
+    "CourseModulePlan",
     "ExecutionStatus",
+    "LearningDestination",
+    "LearningDestinationKind",
+    "LearningPathEngine",
+    "LearningPathRecommendation",
+    "LearningPathSnapshot",
     "MasteryState",
     "PythonExecutionRequest",
     "PythonExecutionResult",
     "PythonPolicyError",
     "PythonSubprocessRunner",
+    "RecommendationReason",
     "StudyCycleStage",
 ]
