b2d8c2d9a8653e091bf3e16466c0713b236f7005
JennerFeijoo <65651751+jennerfeijoo@users.noreply.github.com>
2026-08-04 21:56:53 +0200

    Apply Ruff import grouping

--- tests/test_dm857_project_reasoning_ui.py
+++ tests/test_dm857_project_reasoning_ui.py
@@ -12,8 +12,7 @@ from computational_biomedicine_study_hub.i18n.locales import AppLocale
 from computational_biomedicine_study_hub.storage.sqlite_progress_store import (
     SQLiteProgressStore,
 )
-from computational_biomedicine_study_hub.ui.pages import assessments_page
-from computational_biomedicine_study_hub.ui.pages import dm857_capstone_page
+from computational_biomedicine_study_hub.ui.pages import assessments_page, dm857_capstone_page
 
 
 _PROJECT_RESPONSE = """<ARTIFACT>
